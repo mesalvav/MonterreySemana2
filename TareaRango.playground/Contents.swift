3ece853a31b15f427f4776c2d79fdeb53ad6987d
Mario E Salvatierra V <MarioSalvatierra@marios-mbp-2>
2015-12-10 11:41:11 -0500

    prueba 1141

--- TareaRango.playground/Contents.swift
+++ TareaRango.playground/Contents.swift
@@ -4,7 +4,21 @@
 //
 import UIKit
 
-var str = "Hello, playground"
-
-let unidad = 1
-//uno
+//
+let unRangoDeNumeros = 0...100
+//or range
+for numero in unRangoDeNumeros {
+    
+    if numero % 5 == 0 {
+        print("El numero es: \(numero) Bingo")
+    }
+    if numero % 2 == 0 {
+        print("El numero es:  \(numero) y es par")
+    }
+    if numero % 2 != 0 {
+        print("El numero es:  \(numero) y es impar")
+    }
+    if numero > 30 && numero < 40 {
+        print("El numero es:  \(numero) y Viva Swift!!")
+    }
+}
