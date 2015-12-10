ad8e9ac38038b1cde0f0096afd30f03cfbdc541e
Mario E Salvatierra V <MarioSalvatierra@marios-mbp-2>
2015-12-10 12:26:50 -0500

    improved

--- TareaRango.playground/Contents.swift
+++ TareaRango.playground/Contents.swift
@@ -6,20 +6,36 @@ import UIKit
 
 //
 let unRangoDeNumeros = 0...100
-//or range include more 
+//or range include more
+
 
 for numero in unRangoDeNumeros {
+
+    
     
     if numero % 5 == 0 {
-        print("El numero es: \(numero) Bingo")
+        print("Bingo!!! " , terminator: "")
+        //mensaje +=  "Bingo!!! "
     }
+    
+    //mensaje += " El numero " + numero.description
+    print("El numero \(numero) ", terminator: "")
     if numero % 2 == 0 {
-        print("El numero es:  \(numero) y es par")
+        print(" es par.", terminator: "")
+       // mensaje +=  " es par!!! "
     }
     if numero % 2 != 0 {
-        print("El numero es:  \(numero) y es impar")
+        print(" es impar." , terminator: "")
+       // mensaje +=  " es impar!!! "
     }
-    if numero > 30 && numero < 40 {
-        print("El numero es:  \(numero) y Viva Swift!!")
+    
+    if numero >= 30 && numero <= 40 {
+        print(" Viva Swift!!")
+        //mensaje +=  "Viva swift! "
+    } else {
+        print("")
     }
+
+    
+   // print(mensaje)
 }
